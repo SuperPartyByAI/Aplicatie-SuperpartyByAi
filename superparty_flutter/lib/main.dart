b18ea545a94e0d97af47c770c1e2903b8e542ebd
SuperPartyByAI <superpartybyai@gmail.com>
2026-01-03 08:29:55 +0000

    fix(flutter): Add missing cloud_firestore import
    Cauză: DocumentSnapshot și FirebaseFirestore nu erau definite în main.dart (linia 117-118)
    
    Fix: Adăugat import 'package:cloud_firestore/cloud_firestore.dart'
    
    De ce elimină eroarea: Importul face disponibile tipurile DocumentSnapshot și FirebaseFirestore
    
    Fișiere afectate:
    - lib/main.dart (1 linie adăugată)
    
    Validare:
    - Compilare Flutter va trece
    - AuthWrapper va putea folosi StreamBuilder<DocumentSnapshot>
    
    Co-authored-by: Ona <no-reply@ona.com>

--- superparty_flutter/lib/main.dart
+++ superparty_flutter/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
 import 'services/firebase_service.dart';
 import 'services/background_service.dart';
