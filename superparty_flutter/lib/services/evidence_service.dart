f6947f2423a26a24ff373d040dd8bde2f2340e0a
SuperPartyByAI <superpartybyai@gmail.com>
2026-01-05 18:43:08 +0000

    fix: revert to direct imports in evidence_service
    Revert evidence_exports.dart - use direct imports instead
    
    Co-authored-by: Ona <no-reply@ona.com>

--- superparty_flutter/lib/services/evidence_service.dart
+++ superparty_flutter/lib/services/evidence_service.dart
@@ -3,7 +3,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:path/path.dart' as path;
-import '../models/evidence_exports.dart';
+import '../models/evidence_model.dart';
+import '../models/evidence_state_model.dart';
 
 class EvidenceUploadResult {
   final String docId;
